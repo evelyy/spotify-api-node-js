78641249527fd306dde19fab08a1b90a0bc2661d
tomoko <mitchell.heaton@gmail.com>
2020-04-05 05:36:55 +1000

    Made SpotifyApi into a class, added setCredentials

--- src/spotify-api.js
+++ src/spotify-api.js
@@ -1,10 +1,28 @@
+const WebApiRequest = require('./web-api-request');
+
+class SpotifyApi {
+  constructor(credentials) {
+    this._credentials = credentials || {};
+  }
+}
+
 SpotifyApi.prototype = {
+  setCredentials: (credentials) => {
+    for (let key in credentials) {
+      if (credentials.hasOwnProperty(key)) {
+        this._credentials[key] = credentials[key];
+      };
+    };
+  },
+
+
 
   /**
    * 
    * @param {string} playlistID The playlist's ID
    * @param {string} image Base64 encoded JPEG image data, maximum size 256 KB
-   */
+   * 
+   **/
   changePlaylistCover: function(playlistID, file) {
 
     return WebApiRequest.builder(this.getAccessToken())
